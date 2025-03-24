520e6752c19f0385b0067603d7fea1ceba3de6cb
Sin7667 <sanaz.dt9776@gmail.com>
2025-03-24 10:55:34 +0100

    Code Structure Ergänzung

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams , Stack} from 'expo-router';
  
 
 const ProductDetailsScreen = () => {
     const { id } = useLocalSearchParams();
     return (
         <View>
+            <Stack.Screen options={{ title: 'Product Details' + id }}/>
             <Text>ProductListItem for id : {id} </Text>
         </View>
     )
