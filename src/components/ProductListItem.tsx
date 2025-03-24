8c7435af4db07bb263bfa28591a7591d59cb6485
Sin7667 <sanaz.dt9776@gmail.com>
2025-03-24 10:46:03 +0100

    Code Structure for Screens

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -14,17 +14,14 @@ type ProductListItemProps= {
 const ProductListItem= ({ product }: ProductListItemProps) => {
 
   return (
-    <Link href={`/${product.id}`} asChild>
-     
-
+    <Link href={`/menu/${product.id}`} asChild>
       <Pressable style={styles.container}>
       <Image source={{ uri: product.image || defualtPizzaImage }}
       style={styles.image}
       resizeMode="contain"/>
-
-        <Text style={styles.title}>{product.name}</Text>
-        <Text style={styles.price}>${product.price}</Text>
-       </Pressable>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
     </Link>
   )
 
